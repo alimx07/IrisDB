0077c24715ca107c244e70e13572255655eb1cb9
https://github.com/alimx07 <149194464+alimx07@users.noreply.github.com>
2026-04-15 10:04:35 +0200

    variables

--- variables.go
+++ variables.go
@@ -6,29 +6,31 @@ import (
 )
 
 var (
-	Fsync            = true
-	SyncInterval     = 100 * time.Millisecond
-	PageSize         = 4096
-	Compression      = true
-	Wal              = true
-	Flag             = os.O_CREATE | os.O_RDWR
-	Permission       = 0644
-	FalsePostiveProb = 0.01
-	DBName           = "irisdb"
-	MemTableSize     = 64 * 1024
-	AvgKeySize       = 16
-	SstableSize      = 128 * 1024 // size of sstable in level 0
-	SizeMultiple     = 5          // SizeLevel(i) = Multiple * SizeLevel(i-1)
-	TOMPOSTONE       = []byte{0xFD, 0xFE, 0xFA, 0xF9}
-	MaxLevels        = 6
+	Fsync              = true
+	SyncInterval       = 100 * time.Millisecond
+	PageSize           = 4096
+	Compression        = true
+	Wal                = true
+	Flag               = os.O_CREATE | os.O_RDWR
+	Permission         = 0644
+	FalsePostiveProb   = 0.01
+	DBName             = "irisdb"
+	MemTableSize       = 64 * 1024
+	AvgKeySize         = 16
+	SstableSize        = 128 * 1024 // size of sstable in level 0
+	SizeMultiple       = 5          // SizeLevel(i) = Multiple * SizeLevel(i-1)
+	TOMPOSTONE         = []byte{0xFD, 0xFE, 0xFA, 0xF9}
+	MaxLevels          = 6
+	CompactionInterval = 200 * time.Millisecond
 )
 
 const (
-	KeyExtension     = ".key"
-	ValExtension     = ".val"
-	BloomExtension   = ".bf"
-	WalExtension     = ".wal"
-	SSTABLEExtesnion = ".sst"
-	DBExtension      = ".irisdb"
-	MagicNumber      = 0xAB75DE95
+	KeyExtension   = ".key"
+	ValExtension   = ".val"
+	BloomExtension = ".bf"
+	WalExtension   = ".wal"
+	MagicNumber    = 0xA240702A
+
+	OpPut    byte = 0x01
+	OpDelete byte = 0x02
 )
